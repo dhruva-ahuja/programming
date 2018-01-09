5a308ba2d98b0daef4bdefb12b565c0c578a8d3a
Dhruva Ahuja <dhruva.ahuja@greenstack.in>
2018-01-09 15:26:52 +0530

    Leetcode Trie implementation error input is added.

---
+++ LinkedList/Implementation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+struct Node {
+    T val;
+    Node<T> *next;
+};
+
+void insert(Node<T> **head, int n) {
+    if(*node == NULL) {
+        *node = new 
+    }
+}
+
+int main() {
+
+}
